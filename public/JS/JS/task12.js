491e32c6aaeb5bf8ed6373e2b218874d5f4b16f5
Taz-Castle <B01817384@studentmail.uws.ac.uk>
2025-10-16 12:34:02 +0100

    most up to date
    week 5

--- public/JS/JS/task12.js
+++ public/JS/JS/task12.js
@@ -3,11 +3,11 @@ var faces =[6]
 function preload()
 {
     faces[0] = new Image(); faces[0].src = "Images/dice-1.png"
-    faces[1] = new Image(); faces[0].src = "Images/dice-2.png"
-    faces[2] = new Image(); faces[0].src = "Images/dice-3.png"
-    faces[3] = new Image(); faces[0].src = "Images/dice-4.png"
-    faces[4] = new Image(); faces[0].src = "Images/dice-5.png"
-    faces[5] = new Image(); faces[0].src = "Images/dice-6.png"
+    faces[1] = new Image(); faces[1].src = "Images/dice-2.png"
+    faces[2] = new Image(); faces[2].src = "Images/dice-3.png"
+    faces[3] = new Image(); faces[3].src = "Images/dice-4.png"
+    faces[4] = new Image(); faces[4].src = "Images/dice-5.png"
+    faces[5] = new Image(); faces[5].src = "Images/dice-6.png"
 }
 
 function roll(imgElement)
